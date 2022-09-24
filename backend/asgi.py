1f103eb6de1ecc14f1e928fc299a924d9fc28d5b
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-24 18:59:37 +0530

    Settings in .env

--- backend/asgi.py
+++ backend/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from decouple import config
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS'))
 
 application = get_asgi_application()
